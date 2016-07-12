02851c998cf97e9f1b835f250388d1ec23b45f3f
Mickael Istria <mistria@redhat.com>
2016-07-12 18:31:54 +0200

    Additional documentation about starting VSCode LS

--- org.eclipse.languageserver/src/org/eclipse/languageserver/LanaguageServiceAccessor.java
+++ org.eclipse.languageserver/src/org/eclipse/languageserver/LanaguageServiceAccessor.java
@@ -12,9 +12,12 @@ public class LanaguageServiceAccessor {
 	public static JsonBasedLanguageServer getLanaguageServer() throws IOException {
 		if (server == null) {
 			// Steps to get a usable Language server (for language supported by VSCode LS implementations)
-			// 1. Build VSCode as explained in https://github.com/Microsoft/vscode/wiki/How-to-Contribute#build-and-run-from-source
+			// 1. Make VSCode LS [for CSS here] use STDIN/STDOUT. In file extensions/css/server/cssServerMain.ts change connection line to
+			//       let connection: IConnection = createConnection(new StreamMessageReader(process.in), new StreamMessageWriter(process.out));
+			//    and add imports  StreamMessageReader, StreamMessageWriter (as same level as IPCMessageReader/Writer)
+			// 2. Build VSCode as explained in https://github.com/Microsoft/vscode/wiki/How-to-Contribute#build-and-run-from-source
 			//     (don't forget the `npm run watch` after the install script)
-			// 2. Then set language server location and adapt process builder.
+			// 3. Then set language server location and adapt process builder.
 			
 			ProcessBuilder builder = new ProcessBuilder("/usr/bin/node", "/home/mistria/git/vscode/extensions/css/server/out/cssServerMain.js")
 				.directory(new File("/home/mistria/git/vscode/extensions/css/server/out/"));
