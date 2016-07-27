314f2ffb8e8aaf627a2fb7442930759a69dfb1a6
Mickael Istria <mistria@redhat.com>
2016-07-27 16:12:38 +0200

    Support for open declaration.

--- org.eclipse.languageserver/src/org/eclipse/languageserver/OpenDeclarationHyperlinkDetector.java
+++ org.eclipse.languageserver/src/org/eclipse/languageserver/OpenDeclarationHyperlinkDetector.java
@@ -16,12 +16,14 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.core.filebuffers.FileBuffers;
-import org.eclipse.core.filesystem.EFS;
-import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filebuffers.ITextFileBuffer;
+import org.eclipse.core.filebuffers.ITextFileBufferManager;
+import org.eclipse.core.filebuffers.LocationKind;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
@@ -29,13 +31,10 @@ import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 import io.typefox.lsapi.Location;
@@ -72,31 +71,28 @@ public class OpenDeclarationHyperlinkDetector extends AbstractHyperlinkDetector 
 
 		@Override
 		public void open() {
-			IEditorInput input = null; 
+			IEditorPart part = null;
+			IDocument targetDocument = null;
 			IResource targetResource = LanguageServerEclipseUtils.findResourceFor(this.location.getUri());
-			if (targetResource != null && targetResource.getType() == IResource.FILE) {
-				input = new FileEditorInput((IFile)targetResource);
-			} else {
-				IFileStore fileStore = EFS.getLocalFileSystem().getStore(fileUri);
-				if (fileStore != null) {
-					input = new FileStoreEditorInput(fileStore);
-				}
-			}
-			if (input == null) {
-				return;
-			}
 			try {
-				IEditorPart part = IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), input, null);
-				if (part instanceof AbstractTextEditor) {
-					AbstractTextEditor editor = (AbstractTextEditor) part;
-					IDocument targetDocument = editor.getAdapter(IDocument.class);
-					int offset = LanguageServerEclipseUtils.toOffset(location.getRange().getStart(), targetDocument);
-					int endOffset = LanguageServerEclipseUtils.toOffset(location.getRange().getEnd(), targetDocument);
-					editor.getSelectionProvider().setSelection(new TextSelection(offset, endOffset - offset));
+				if (targetResource != null && targetResource.getType() == IResource.FILE) {
+					part = IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), (IFile)targetResource);
+					targetDocument = ITextFileBufferManager.DEFAULT.getTextFileBuffer(targetResource.getFullPath(), LocationKind.IFILE).getDocument();
+				} else {
+					part = IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), fileUri, null, true);
+					targetDocument = ITextFileBufferManager.DEFAULT.getTextFileBuffer(new Path(fileUri.getPath()), LocationKind.LOCATION).getDocument();
 				}
 			} catch (PartInitException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			}
+			try {
+				if (part instanceof AbstractTextEditor) {
+					AbstractTextEditor editor = (AbstractTextEditor) part;
+					int offset = LanguageServerEclipseUtils.toOffset(location.getRange().getStart(), targetDocument);
+					int endOffset = LanguageServerEclipseUtils.toOffset(location.getRange().getEnd(), targetDocument);
+					editor.getSelectionProvider().setSelection(new TextSelection(offset, endOffset > offset ? endOffset - offset : 0));
+				}
 			} catch (BadLocationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -121,10 +117,10 @@ public class OpenDeclarationHyperlinkDetector extends AbstractHyperlinkDetector 
 			if (server != null) {
 				CompletableFuture<List<? extends Location>> documentHighlight = server.getTextDocumentService().definition(LanguageServerEclipseUtils.toTextDocumentPosistionParams(fileUri, region.getOffset(), textViewer.getDocument()));
 				List<? extends Location> response = documentHighlight.get();
-				List<IHyperlink> hyperlinks = new ArrayList<IHyperlink>(response.size());
-				if (hyperlinks.isEmpty()) {
+				if (response.isEmpty()) {
 					return null;
 				}
+				List<IHyperlink> hyperlinks = new ArrayList<IHyperlink>(response.size());
 				for (Location responseLocation : response) {
 					hyperlinks.add(new LSBasedHyperlink(responseLocation, fileUri, region));
 				}
