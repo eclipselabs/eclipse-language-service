29fff1f6eeef895fee2096392ff5e28da61a1197
Mickael Istria <mistria@redhat.com>
2016-10-28 05:20:24 +0200

    Minor improvement in C# PresentationReconcilier
    Add literal numbers and identifier rule

--- org.eclipse.languageserver.languages/src/org/eclipse/languageserver/languages/csharp/CSharpKeywordsHighlighter.java
+++ org.eclipse.languageserver.languages/src/org/eclipse/languageserver/languages/csharp/CSharpKeywordsHighlighter.java
@@ -23,6 +23,7 @@ import org.eclipse.jface.text.rules.IRule;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.IWordDetector;
 import org.eclipse.jface.text.rules.MultiLineRule;
+import org.eclipse.jface.text.rules.NumberRule;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.rules.SingleLineRule;
 import org.eclipse.jface.text.rules.Token;
@@ -141,29 +142,53 @@ public class CSharpKeywordsHighlighter extends PresentationReconciler {
 	public CSharpKeywordsHighlighter() {
 		RuleBasedScanner scanner= new RuleBasedScanner();
 		Token keywordToken = new Token(new TextAttribute(Display.getDefault().getSystemColor(SWT.COLOR_DARK_BLUE), null, SWT.BOLD));
-		WordRule wordRule = new WordRule(new IWordDetector() {
+		Set<Character> allKeyWordChars = new HashSet<Character>();
+		Set<Character> allKeyWord1stChar = new HashSet<Character>();
+		for (String keyword : keywords) {
+			if (!keyword.isEmpty()) {
+				allKeyWord1stChar.add(keyword.charAt(0));
+				for (char c : keyword.toCharArray()) {
+					allKeyWordChars.add(Character.valueOf(c));
+				}
+			}
+		}
+		WordRule keywordRule = new WordRule(new IWordDetector() {
 			@Override
 			public boolean isWordStart(char c) {
-				return Character.isAlphabetic(c);
+				return allKeyWord1stChar.contains(c);
 			}
 			
 			@Override
 			public boolean isWordPart(char c) {
-				return Character.isAlphabetic(c) || Character.isDigit(c);
+				return allKeyWordChars.contains(c);
 			}
 		});
 		for (String keyword : keywords) {
-			wordRule.addWord(keyword, keywordToken);
+			keywordRule.addWord(keyword, keywordToken);
 		}
-		Token stringToken = new Token(new TextAttribute(Display.getDefault().getSystemColor(SWT.COLOR_BLUE), null, SWT.NONE));
+		Token identifierToken = new Token(new TextAttribute(Display.getDefault().getSystemColor(SWT.COLOR_BLACK), null, SWT.NONE));
+		WordRule identifierRule = new WordRule(new IWordDetector() {
+			@Override
+			public boolean isWordStart(char c) {
+				return Character.isJavaIdentifierStart(c);
+			}
+			
+			@Override
+			public boolean isWordPart(char c) {
+				return Character.isJavaIdentifierPart(c);
+			}
+		}, identifierToken);
+		Token literalToken = new Token(new TextAttribute(Display.getDefault().getSystemColor(SWT.COLOR_BLUE), null, SWT.NONE));
 		Token commentsToken = new Token(new TextAttribute(Display.getDefault().getSystemColor(SWT.COLOR_DARK_GRAY), null, SWT.NONE));
 		scanner.setRules(new IRule[] {
-			wordRule,
-			new SingleLineRule("\"", "\"", stringToken),
-			new SingleLineRule("'", "'", stringToken),
-			new MultiLineRule("@\"", "\"", stringToken, '"'),
 			new SingleLineRule("//", null, commentsToken),
-			new MultiLineRule("/*", "*/", commentsToken)
+			new MultiLineRule("/*", "*/", commentsToken),
+			new SingleLineRule("\"", "\"", literalToken),
+			new SingleLineRule("'", "'", literalToken),
+			new MultiLineRule("@\"", "\"", literalToken, '"'),
+			keywordRule,
+			identifierRule,
+			new NumberRule(literalToken),
 		});
 		DefaultDamagerRepairer dr= new DefaultDamagerRepairer(scanner);
 		this.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
