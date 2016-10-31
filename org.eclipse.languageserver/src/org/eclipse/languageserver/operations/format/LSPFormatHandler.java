5bc7aab381f2a13a8115edc77f3b7aeb08ec71ff
Michal Niewrzal <michal.n@zend.com>
2016-10-31 15:30:07 +0100

    Formatting improvements
    * FormattingOptions added to list of parameters
    * fixed problem with changes order
    * applied changes can be undone as single change

--- org.eclipse.languageserver/src/org/eclipse/languageserver/operations/format/LSPFormatHandler.java
+++ org.eclipse.languageserver/src/org/eclipse/languageserver/operations/format/LSPFormatHandler.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *  Mickael Istria (Red Hat Inc.) - initial implementation
+ *  Michał Niewrzał (Rogue Wave Software Inc.)
  *******************************************************************************/
 package org.eclipse.languageserver.operations.format;
 
@@ -25,6 +26,12 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.languageserver.LSPEclipseUtils;
 import org.eclipse.languageserver.LanguageServiceAccessor;
 import org.eclipse.languageserver.LanguageServiceAccessor.LSPDocumentInfo;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.text.edits.MalformedTreeException;
+import org.eclipse.text.edits.MultiTextEdit;
+import org.eclipse.text.edits.ReplaceEdit;
+import org.eclipse.text.undo.DocumentUndoManagerRegistry;
+import org.eclipse.text.undo.IDocumentUndoManager;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
@@ -36,10 +43,9 @@ import io.typefox.lsapi.DocumentFormattingParams;
 import io.typefox.lsapi.ServerCapabilities;
 import io.typefox.lsapi.TextEdit;
 import io.typefox.lsapi.builders.DocumentFormattingParamsBuilder;
+import io.typefox.lsapi.builders.FormattingOptionsBuilder;
 
 public class LSPFormatHandler extends AbstractHandler implements IHandler {
-
-	private IDocument document;
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -49,19 +55,16 @@ public class LSPFormatHandler extends AbstractHandler implements IHandler {
 			if (info != null) {
 				ISelection sel = ((AbstractTextEditor) part).getSelectionProvider().getSelection();
 				if (sel instanceof TextSelection) {
+					final Shell shell = HandlerUtil.getActiveShell(event);
 					DocumentFormattingParams params = new DocumentFormattingParamsBuilder()
-							.textDocument(info.getFileUri().toString())
-							.build();
+					        .textDocument(info.getFileUri().toString())
+					        .options(new FormattingOptionsBuilder().build())
+					        .build();
 					CompletableFuture<List<? extends TextEdit>> formatter = info.getLanguageClient().getTextDocumentService().formatting(params);
 					formatter.thenAccept((List<? extends TextEdit> t) -> {
-						for (TextEdit textEdit : t) {
-							try {
-								LSPEclipseUtils.applyEdit(textEdit, document);
-							} catch (BadLocationException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
+						shell.getDisplay().asyncExec(() -> {
+							applyChanges(info.getDocument(), t);
+						});
 					});
 				}
 			}
@@ -69,11 +72,44 @@ public class LSPFormatHandler extends AbstractHandler implements IHandler {
 		return null;
 	}
 
+	private void applyChanges(IDocument document, List<? extends TextEdit> t) {
+		if (document == null || t.isEmpty()) {
+			return;
+		}
+
+		IDocumentUndoManager manager = DocumentUndoManagerRegistry.getDocumentUndoManager(document);
+		if (manager != null) {
+			manager.beginCompoundChange();
+		}
+
+		MultiTextEdit edit = new MultiTextEdit();
+		for (TextEdit textEdit : t) {
+			try {
+				int offset = LSPEclipseUtils.toOffset(textEdit.getRange().getStart(), document);
+				int length = LSPEclipseUtils.toOffset(textEdit.getRange().getEnd(), document) - offset;
+				edit.addChild(new ReplaceEdit(offset, length, textEdit.getNewText()));
+			} catch (BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		try {
+			edit.apply(document);
+		} catch (MalformedTreeException | BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (manager != null) {
+			manager.endCompoundChange();
+		}
+	}
+
 	@Override
 	public boolean isEnabled() {
 		IWorkbenchPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
 		if (part instanceof ITextEditor) {
-			LSPDocumentInfo info = LanguageServiceAccessor.getLSPDocumentInfoFor((ITextEditor) part, ServerCapabilities::isDocumentFormattingProvider);
+			LSPDocumentInfo info = LanguageServiceAccessor.getLSPDocumentInfoFor((ITextEditor) part,
+			        ServerCapabilities::isDocumentFormattingProvider);
 			ISelection selection = ((ITextEditor) part).getSelectionProvider().getSelection();
 			return info != null && !selection.isEmpty() && selection instanceof ITextSelection;
 		}
